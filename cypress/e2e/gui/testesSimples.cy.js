8f79850d8f0dcf23e882b1cdfa8c8bd7618b6a1b
Guilherme de Almeida Pastl <guilherme.pastl@seventh.com.br>
2023-05-13 23:19:02 -0300

    Testes integrados de GUI e API

--- cypress/e2e/gui/testesSimples.cy.js
+++ cypress/e2e/gui/testesSimples.cy.js
@@ -8,7 +8,7 @@ describe('Suite de teste básico/simples', () => {
     cy.get('.blank-state-welcome-title').should('be.visible').and('contains.text', 'Welcome to GitLab')
   })
 
-  it.only('2. Criação e exclusão de projeto via GUI com sucesso', () => {
+  it('2. Criação e exclusão de projeto via GUI com sucesso', () => {
     cy.login()
     cy.contains('Create a project').click()
     cy.get('#project_name').type('Projeto teste')
@@ -24,5 +24,26 @@ describe('Suite de teste básico/simples', () => {
     cy.get('.flash-notice > span')
       .should('be.visible')
       .and('have.text', "Project 'Administrator / Projeto teste' is in the process of being deleted.")
+  })
+
+  it.only('3. Criar uma label e adicionar à issue', () => {
+    cy.visit('/') // visits the baseUrl
+    cy.get('#user_login').type('root')
+    cy.get('#user_password').type('12345678')
+    cy.get('#new_user > .submit-container > .btn').click()
+    cy.get('.project-name').click()
+    cy.get('.shortcuts-issues').trigger('mouseover')
+    cy.contains('Labels').click({force:true})
+    cy.get('#new_label_link').click()
+    cy.get('#label_title').type('Etiqueta exemplo')
+    cy.get('#label_description').type('Descrição da etiqueta')
+    cy.get('.form-actions > .btn-success').click()
+    cy.get('.shortcuts-issues').click()
+    cy.get('.issue-title-text > a').click()
+    cy.get('.labels > .title > .js-sidebar-dropdown-toggle').click()
+    cy.get('.label-item').click()
+    cy.get('.content-wrapper').click()
+    cy.get('.dont-hide > a > .badge')
+      .should('be.visible').and('have.text', 'Etiqueta exemplo')
   })
 })
--- cypress/support/api_commands.js
+++ cypress/support/api_commands.js
@@ -19,9 +19,9 @@ Cypress.Commands.add('api_getAllProjects', () => {
       url: '/api/v4/projects/',
       headers: { Authorization: accessToken },
     })
-  })
+})
   
-  Cypress.Commands.add('api_deleteProjects', () => {
+Cypress.Commands.add('api_deleteProjects', () => {
     cy.api_getAllProjects().then(res =>
       res.body.forEach(project => cy.request({
         method: 'DELETE',
@@ -29,4 +29,31 @@ Cypress.Commands.add('api_getAllProjects', () => {
         headers: { Authorization: accessToken },
       }))
     )
+})
+
+Cypress.Commands.add('api_createIssue', issue => {
+  cy.api_createProject(issue.project)
+    .then(response => {
+      cy.request({
+        method: 'POST',
+        url: `/api/v4/projects/${response.body.id}/issues`,
+        body: {
+          title: issue.title,
+          description: issue.description
+        },
+        headers: { Authorization: accessToken },
+      })
   })
+})
+
+Cypress.Commands.add('api_createLabel', (projectId, label) => {
+  cy.request({
+    method: 'POST',
+    url: `/api/v4/projects/${projectId}/labels`,
+    body: {
+      name: label.name,
+      color: label.color
+    },
+    headers: { Authorization: accessToken },
+  })
+})
